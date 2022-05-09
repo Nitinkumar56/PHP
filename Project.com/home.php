bfd6a1c3d245124df7638a88883c65192b0171f8
nitin <nk2342001@gmail.com>
2022-05-08 18:19:55 -0700

    delete

--- Project.com/home.php
+++ Project.com/home.php
@@ -11,10 +11,96 @@
     integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 
   <title>how-healthy.com</title>
+  <style>
+    .p11:hover{
+      background-color:rgb(255, 0, 0);
+      font-size:30px;
+      font-family:bookman;
+    }
+    .p11{
+      font-size:29px;
+      font-family:palatino;
+      
+      width: 150px;
+    }
+  </style>
 </head>
 
 <body>
-  <?php include"header.php" ?>
+<nav class="navbar navbar-expand-lg navbar-dark fixed-top bg-warning">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="#"><img src="img/logo.png" height="70"></a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
+        aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+    </div>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent" align="center">
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+          <li class="nav-item p11 rounded-pill mx-2">
+            <a class="nav-link active" aria-current="page" href="index.php">Home</a>
+          </li>
+          <li class="nav-item p11 rounded-pill mx-2">
+            <a class="nav-link active" href="about.php">about</a>
+          </li>
+          <li class="nav-item p11 rounded-pill mx-2">
+            <a class="nav-link active" href="contact.php">Contact us</a>
+          </li>
+
+        </ul>
+      </div>
+  </nav>
+
+  <div id="carouselExampleCaptions" class="carousel slide  .carousel-fade" data-bs-ride="carousel">
+    <div class="carousel-indicators">
+      <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active"
+        aria-current="true" aria-label="Slide 1"></button>
+      <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1"
+        aria-label="Slide 2"></button>
+      <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2"
+        aria-label="Slide 3"></button>
+    </div>
+    <div class="carousel-inner">
+      <div class="carousel-item active">
+        <img src="img/wraper.jpg" class="d-block w-100" height="500" alt="...">
+        <div class="carousel-caption d-none d-md-block">
+          <h2>Wellcome to iCoder</h2>
+          <p>Technology News, Development and Treads</p>
+          <button class="btn btn-danger">Technology</button>
+          <button class="btn btn-warning">Web Development</button>
+          <button class="btn btn-success">Tech Fun</button>
+        </div>
+      </div>
+      <div class="carousel-item">
+        <img src="img/wraper.jpg" class="d-block w-100" height="500" alt="...">
+        <div class="carousel-caption d-none d-md-block">
+          <h2>The Best Coding Blog</h2>
+          <p>Technology News, Development and Treads</p>
+          <button class="btn btn-danger">Technology</button>
+          <button class="btn btn-warning">Web Development</button>
+          <button class="btn btn-success">Tech Fun</button>
+        </div>
+      </div>
+      <div class="carousel-item">
+        <img src="img/wraper.jpg" class="d-block w-100" height="500" alt="...">
+        <div class="carousel-caption d-none d-md-block">
+          <h2>Award winning Blog</h2>
+          <p>Technology News, Development and Treads</p>
+          <button class="btn btn-danger">Technology</button>
+          <button class="btn btn-warning">Web Development</button>
+          <button class="btn btn-success">Tech Fun</button>
+        </div>
+      </div>
+    </div>
+    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
+      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+      <span class="visually-hidden">Previous</span>
+    </button>
+    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
+      <span class="carousel-control-next-icon" aria-hidden="true"></span>
+      <span class="visually-hidden">Next</span>
+    </button>
+  </div>
   <div class="container my-5">
   <h2>Want to become a healthier person?Follow these tips & live a long and happy life.. </h2>
     <p>Do you want to live a long life? Are you healthy? Take this advice: We should eat vegetables and fruits, some fish, like tuna and salmon, drink lots of water, and use healthy oils, like olive oil, canola oil. We should take a walk, as much as possible. We should avoid tobacco, alcohol, drugs.
@@ -131,7 +217,48 @@
     </div>
   </div>
   
-  <?php include"footer.php" ?>
+  
+  <div class="container my-4">
+    <div class="row mb-2">
+      <div class="col-md-6">
+        <div class="row g-0 border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative">
+          <div class="col p-4 d-flex flex-column position-static">
+            <strong class="d-inline-block mb-2 text-danger">Weight lose</strong>
+            <h3 class="mb-0">How to Lose Weight</h3>
+            <div class="mb-1 text-muted">Nov 12</div>
+            <p class="card-text mb-auto">Working on weight loss? Then you probably want results -- fast.<br />
+        Let me save you some time: skip the fad diets....</p>
+            <a href="#" class="stretched-link">Continue reading</a>
+          </div>
+          <div class="col-auto d-none d-lg-block">
+            <img class="bd-placeholder-img" width="200" height="250"
+              src="https://source.unsplash.com/1400x500/?code,laptop">
+          </div>
+        </div>
+      </div>
+      <div class="col-md-6">
+        <div class="row g-0 border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative">
+          <div class="col p-4 d-flex flex-column position-static">
+            <strong class="d-inline-block mb-2 text-warning">Yourself Healthy</strong>
+            <h3 class="mb-0">Keeping Yourself Healthy </h3>
+            <div class="mb-1 text-muted">Nov 11</div>
+            <p class="mb-auto">Look up. The sun is shining, the birds are singing, and the sky is blue. How beautiful
+               a world it is! How do you enjoy your life in such a wonderful world?...</p>
+            <a href="#" class="stretched-link">Continue reading</a>
+          </div>
+          <div class="col-auto d-none d-lg-block">
+            <img class="bd-placeholder-img" width="200" height="250"
+              src="https://source.unsplash.com/1400x500/?nature,tree">
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <footer class="container bg-warning">
+    <p class="float-end"><a href="#">Back to top</a></p>
+    <p>© 2021–2022 Nitin,<a href="index.html">how-healthy.com</a> Inc. · <a href="#">Privacy</a> . <a href="#">Terms</a></p>
+  </footer>
+
 
   <!-- Optional JavaScript; choose one of the two! -->
 
