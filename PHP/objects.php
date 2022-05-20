3614bd5b0b7fe194534bab993ea4eaab5acfb9a1
nitin <nk2342001@gmail.com>
2022-05-20 12:11:21 -0700

    php

---
+++ PHP/objects.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    class Person {
+        public $age;
+        function speak() {
+            echo "Hello !";
+        }
+    }
+    $p1 = new Person();
+    $p1->age =23;
+    echo $p1->age;
+    $p1->speak();
+?>
+</body>
+</html>
